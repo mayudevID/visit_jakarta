456fb844137fb388f73eebf3f4f099a8e9487dbf
Maulana Yusuf <ymaulana089@gmail.com>
2022-05-27 21:28:14 +0700

    make partials (navbar)

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -21,41 +21,7 @@
     <i class="uil uil-arrow-up"></i>
   </button>
   <!-- Akhir scroll top button -->
-  <header>
-    <nav class="navbar navbar-expand-lg fw-semibold fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="#">Logo</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-              <a class="nav-link actived" aria-current="page" href="#">Beranda</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Museum</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Galeri</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Kuliner</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Tempat Rekreasi</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#"><i class="uil uil-search"></i></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#"><button type="button" class="btn btn-purple">Register/Login</button></a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  </header>
+  @include('partials.navbar')
 
   <main>
     <!-- Welcome -->

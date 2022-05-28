6d588fb5cfddd47509f669eb59b3760ce3a21a5d
Maulana Yusuf <ymaulana089@gmail.com>
2022-05-28 19:59:26 +0700

    add bootstrap ui

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('beranda');
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
